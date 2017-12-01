87c09409feb0347947738c076206d16a392ea9aa
Jessie Adkins <zaheelee@gmail.com>
2017-12-01 11:47:14 -0500

    about to write rcvMsg

--- Project2/distMeasurement.py
+++ Project2/distMeasurement.py
@@ -6,11 +6,55 @@ def readSitesFromFile():
     siteList = [x.strip() for x in siteList]
     return siteList
 
+def sendMsg(sendSocket, dest_ip, dest_port):
+    msg = "measurement for class project. questions to student jsa70@case.edu or professor mxr136@case.edu"
+    payload = bytes(msg + 'a' * (1472 - len(msg)), 'ascii')
+    sendSocket.sendto(payload, (dest_ip, dest_port))
+    return
+
+#TODO
+def rcvMsg(rcvSocket, timeout):
+    timeLeft = timeout
+
+    while True:
+        startTime = time.clock()
+
+        
+
+        endTime = time.clock()
+        timeLeft = timeLeft - (endTime - startTime)
+        if timeLeft <= 0:
+            print("timeout")
+            return
+    return
+
+#TODO
+def measureOne(dest_addr):
+    dest_ip = socket.gethostbyname(dest_addr)
+    dest_port = 80 #check if this is an okay port to use (expecting an ICMP error)
+    
+    sendSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #TODO format datagram socket header fields
+
+    sendMsg(sendSocket, dest_ip, dest_port)
+    sendSocket.close()
+
+    rcvSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    rcvMsg(rcvSocket, 10)
+    rcvSocket.close()
+    return
+
+#TODO
+def measureAll():
+    print
+
 def main():
     siteList = readSitesFromFile()
     print("Input websites: ")
     print(siteList)
     #TODO
+
+    measureOne(siteList[0])
 
     #TODO CHANGE THIS
     return 0
